699c57b81f8a3df1eff58d674a1f3762b9276c9d
phanbadu <duphan03@gmail.com>
2024-06-24 02:45:59 +0700

    Kết nối mongodb và sử dụng mô hình mvc và test api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import epxress from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auth.route.js';
+
+dotenv.config();
 
 const app = epxress();
 
-app.listen(3001, () => {
-    console.log('Server is running on port 3001!');
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 3001;
+
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log('MONGODB IS CONNECTED!'))
+    .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}!`);
 });
