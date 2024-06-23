e8b6cbaa077fcd16d3d08b1265ad7e06f2282fab
phanbadu <duphan03@gmail.com>
2024-06-24 02:11:00 +0700

    Thêm UI SignIn và SignUp

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,25 @@
+import OAuthGoogle from "../components/OAuthGoogle";
+import { Link } from "react-router-dom";
 
 export default function SignIn() {
   return (
-    <div>
-      SignIn
+    <div className="w-full h-screen p-20 text-white">
+      <div className="w-full h-full bg-gray-900 flex justify-center items-center rounded-lg">
+        <div className="w-[500px] flex flex-col gap-10">
+          <h1 className="text-4xl font-bold">ĐĂNG NHẬP</h1>
+          <OAuthGoogle />
+          <span className="text-center">- OR -</span>
+          <form className="w-full flex flex-col gap-10">
+            <input className="w-full text-sm focus:border-white duration-300 bg-gray-900 outline-none border-b-2 border-white/40" placeholder="Nhập email" type="email" />
+            <input className="w-full text-sm focus:border-white duration-300 bg-gray-900 outline-none border-b-2 border-white/40" placeholder="Nhập mật khẩu" type="password" />
+            <button className="bg-gray-950 p-2 rounded-lg duration-200 hover:bg-gray-950/60">Đăng nhập</button>
+          </form>
+          <span className="text-sm flex gap-2">
+            Bạn chưa có tài khoản ?
+            <Link className="text-sky-500 hover:underline duration-300" to={"/signup"}>Đăng ký</Link>
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,26 @@
+import OAuthGoogle from "../components/OAuthGoogle";
+import { Link } from "react-router-dom"
 
 export default function SignUp() {
   return (
-    <div>
-      SignUp
+    <div className="w-full h-screen p-20 text-white">
+      <div className="w-full h-full bg-gray-900 flex justify-center items-center rounded-lg">
+        <div className="w-[500px] flex flex-col gap-10">
+          <h1 className="text-4xl font-bold">ĐĂNG KÝ</h1>
+          <OAuthGoogle />
+          <span className="text-center">- OR -</span>
+          <form className="w-full flex flex-col gap-10">
+            <input className="w-full text-sm focus:border-white duration-300 bg-gray-900 outline-none border-b-2 border-white/40" placeholder="Nhập họ tên" type="text" />
+            <input className="w-full text-sm focus:border-white duration-300 bg-gray-900 outline-none border-b-2 border-white/40" placeholder="Nhập email" type="email" />
+            <input className="w-full text-sm focus:border-white duration-300 bg-gray-900 outline-none border-b-2 border-white/40" placeholder="Nhập mật khẩu" type="password" />
+            <button className="bg-gray-950 p-2 rounded-lg duration-200 hover:bg-gray-950/60">Đăng ký</button>
+          </form>
+          <span className="text-sm flex gap-2">
+            Bạn đã có tài khoản ?
+            <Link className="text-sky-500 hover:underline duration-300" to={"/signin"}>Đăng nhập</Link>
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
